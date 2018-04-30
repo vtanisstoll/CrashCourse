8b2037c13b8890db7ec6401fa4bdd412207f07d5
Victor Stoll <vtanisstoll@gmail.com>
2018-04-30 11:25:33 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Document</title>
+	</head>
+	<body>
+		<input type="firstName" id="fn">
+		<button>process this</button>
+		<p id="result"></p>
+		<script>
+		</script>
+	</body>
+</html>
